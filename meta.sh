fbc6d5d4ce9e374f682d7f3dec40de0b84359d8b
Hydra NET <67011201+hydra20@users.noreply.github.com>
2020-07-05 13:07:43 +0100

    Add files via upload
    install metasploit automatically

---
+++ meta.sh
@@ -0,0 +1,29 @@
+printf "\n"
+printf "\e[1;31m hi Hacker I am Hydra net welcome to my tool 
+for install metasploit automatically" 
+printf "\e[1;35m
+
+
+███╗░░░███╗███████╗████████╗░█████╗░░░░░█████╗░██╗░░░██╗████████╗░█████╗░
+████╗░████║██╔════╝╚══██╔══╝██╔══██╗░░░██╔══██╗██║░░░██║╚══██╔══╝██╔══██╗
+██╔████╔██║█████╗░░░░░██║░░░███████║░░░███████║██║░░░██║░░░██║░░░██║░░██║
+██║╚██╔╝██║██╔══╝░░░░░██║░░░██╔══██║██╗██╔══██║██║░░░██║░░░██║░░░██║░░██║
+██║░╚═╝░██║███████╗░░░██║░░░██║░░██║╚█║██║░░██║╚██████╔╝░░░██║░░░╚█████╔╝
+╚═╝░░░░░╚═╝╚══════╝░░░╚═╝░░░╚═╝░░╚═╝░╚╝╚═╝░░╚═╝░╚═════╝░░░░╚═╝░░░░╚════╝░"
+sleep 6
+
+printf "\n"
+printf "\e1;30m" 
+pkg install root-repo
+pkg install unstable-repo
+pkg install x11-repo
+pkg update && pkg upgrade && pkg install git curl wget nmap -y && curl -LO raw.githubusercontent.com/Hax4us/Metasploit_termux/master/metasploit.sh && chmod 777 metasploit.sh && ./metasploit.sh
+clear
+printf "\n"
+printf "\e[1;31mThe Installation Was Successful" 
+
+printf "\e[1;32mThank You For Use My Tool
+Dont Forget Subscribe My Channel >Hydra NET<"
+printf "\n" 
+printf "\e[1;31mHave a Great >penetration<" 
+printf "\n" 
