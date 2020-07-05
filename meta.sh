8e574a2831ef8eea3da015fe0b10295043d82280
Hydra NET <67011201+hydra20@users.noreply.github.com>
2020-07-05 13:34:36 +0100

    Update meta.sh

--- meta.sh
+++ meta.sh
@@ -18,6 +18,7 @@ pkg install root-repo
 pkg install unstable-repo
 pkg install x11-repo
 pkg update && pkg upgrade && pkg install git curl wget nmap -y && curl -LO raw.githubusercontent.com/Hax4us/Metasploit_termux/master/metasploit.sh && chmod 777 metasploit.sh && ./metasploit.sh
+bash metasploit.sh 
 clear
 printf "\n"
 printf "\e[1;31mThe Installation Was Successful" 
